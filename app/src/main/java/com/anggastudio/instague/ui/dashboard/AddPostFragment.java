82680f1971c51fabe3a2cd57605b981889d532c4
anggastudio <aa.angga.pratama@gmail.com>
2020-03-20 21:31:18 +0700

    refactor dashboard fragment to add post fragment

---
+++ app/src/main/java/com/anggastudio/instague/ui/dashboard/AddPostFragment.java
@@ -0,0 +1,49 @@
+package com.anggastudio.instague.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.anggastudio.instague.R;
+
+public class AddPostFragment extends Fragment {
+
+    private AddPostViewModel addPostViewModel;
+    private TextView textView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.text_dashboard);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(getActivity() != null){
+            addPostViewModel = new ViewModelProvider(this).get(AddPostViewModel.class);
+            observeViewModel();
+        }
+    }
+
+    private void observeViewModel() {
+        addPostViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+    }
+}
